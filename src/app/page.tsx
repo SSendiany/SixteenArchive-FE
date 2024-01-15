c79893fbc7ceed14e6b2e0f50305dc911053840f
SSendiany <septiantosendiany@gmail.com>
2024-01-16 02:14:27 +0700

    Taaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,12 +69,12 @@ export default function Home() {
                   <h1 className={`text-4xl font-[700] text-white`}>200+</h1>
                   <span className="uppercase">Project</span>
                 </div>
-                <div className="md:border-b-0 border-b-2 md:pb-0 pb-16 md:py-0 w-[15vw] mx-auto border-primaryOrange"></div>
+                <div className="md:hidden border-b-2 md:pb-0 pb-16 md:py-0 w-[15vw] mx-auto border-primaryOrange"></div>
                 <div className="md:border-r-2 md:py-0 py-16 border-primaryOrange">
                   <h1 className={` text-4xl font-[700] text-white`}>80+</h1>
                   <span className="uppercase">Clients</span>
                 </div>
-                <div className="md:border-b-0 border-b-2 md:pb-0 md:py-0 w-[15vw] mx-auto border-primaryOrange"></div>
+                <div className="md:hidden border-b-2 md:pb-0 md:py-0 w-[15vw] mx-auto border-primaryOrange"></div>
                 <div className="md:pt-0 pt-16">
                   <h1 className={` text-4xl font-[700] text-white`}>2+</h1>
                   <span className="uppercase">Years</span>
