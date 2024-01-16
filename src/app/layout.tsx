0cbe7098217c040d596bd56d17700db07ba63a5b
SSendiany <septiantosendiany@gmail.com>
2024-01-16 17:58:29 +0700

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <link rel="icon" href="./favicon.ico" sizes="any" />
       <body className={MetropolisBlack.className}>{children}</body>
     </html>
